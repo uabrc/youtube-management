a16daa6c1d64414c0860ea0c04a6c2add412d043
William Warriner <wwarriner@gmail.com>
2023-04-29 15:34:22 -0500

    alternate impl

--- src/generate_thumbnail_pptx.py
+++ src/generate_thumbnail_pptx.py
@@ -1,10 +1,110 @@
 import collections  # ! supports pptx
-import collections.abc  # ! supports pptx
+import collections.abc  # ! support pptx
 import textwrap
 from pathlib import PurePath
+from typing import Dict
 
 import pandas as pd
 import pptx
+
+# FIELDS
+TITLE = "title"
+SUBTITLE = "subtitle"
+PART = "part"
+CATEGORY = "category"
+INDEX = "index"
+DATE = "date"
+LAYOUT_INDEX = "layout_index"
+
+# LAYOUT
+TITLE_PLACEHOLDER_INDEX = 0
+TITLE_FONT_SIZE = pptx.util.Pt(48)  # type: ignore
+SUBTITLE_PLACEHOLDER_INDEX = 1
+SUBTITLE_FONT_SIZE = pptx.util.Pt(24)  # type: ignore
+
+
+class ContentBuilder:
+    def __init__(self, organization_name: str):
+        self._organization_name: str = organization_name
+
+    def build_content(self, row: pd.Series) -> Dict[str, str]:
+        return {
+            "title": self._build_title_content(row=row),
+            "subtitle": self._build_subtitle_content(row=row),
+        }
+
+    def _build_title_content(self, row: pd.Series) -> str:
+        part = row[PART]
+        part_content = self._build_part_content(part=part)
+        title_content = textwrap.dedent(
+            f"""
+            {row[TITLE]}{part_content}
+            """
+        ).strip()
+        return title_content
+
+    def _build_part_content(self, part: pd.Series) -> str:
+        if not pd.isna(part):
+            part_value = int(str(part))
+            part_content = f" (Part {part_value:d})"
+        else:
+            part_content = ""
+        return part_content
+
+    def _build_subtitle_content(self, row: pd.Series) -> str:
+        category_content = self._build_category_content(row[CATEGORY])
+        index_content = self._build_index_content(row[INDEX])
+
+        date = row[DATE].strftime(r"%Y-%m-%d")
+
+        subtitle_content = textwrap.dedent(
+            f"""
+            {category_content}{index_content}
+            {self._organization_name}
+            {date}
+            """
+        ).strip()
+        return subtitle_content
+
+    def _build_category_content(self, category: pd.Series) -> str:
+        if not pd.isna(category):
+            category_content = str(category)
+        else:
+            category_content = ""
+        return category_content
+
+    def _build_index_content(self, index: pd.Series) -> str:
+        if not pd.isna(index):
+            index_value = int(str(index))
+            index_content = f" #{index_value:d}"
+        else:
+            index_content = ""
+        return index_content
+
+
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    df[PART] = pd.to_numeric(df[PART])
+    df[INDEX] = pd.to_numeric(df[INDEX])
+    df[DATE] = pd.to_datetime(df[DATE])
+    return df
+
+
+def add_content_to_slide(content: Dict[str, str], slide: pptx.Slide):
+    # TITLE
+    title_content = content[TITLE]
+    title_placeholder = slide.placeholders[TITLE_PLACEHOLDER_INDEX]
+    title_placeholder.text = title_content
+    for paragraph in title_placeholder.text_frame.paragraphs:
+        for run in paragraph.runs:
+            run.font.size = TITLE_FONT_SIZE
+
+    # SUBTITLE
+    subtitle_content = content[SUBTITLE]
+    subtitle_placeholder = slide.placeholders[SUBTITLE_PLACEHOLDER_INDEX]
+    subtitle_placeholder.text = subtitle_content
+    for paragraph in subtitle_placeholder.text_frame.paragraphs:
+        for run in paragraph.runs:
+            run.font.size = SUBTITLE_FONT_SIZE
 
 
 def main():
@@ -19,87 +119,22 @@ def main():
     # PRESENTATION CONFIGURATION
     PPTX_16_9_EMU = (12192000, 6868000)
 
-    # LAYOUT
-    TITLE_PLACEHOLDER_INDEX = 0
-    TITLE_FONT_SIZE = pptx.util.Pt(48)  # type: ignore
-    SUBTITLE_PLACEHOLDER_INDEX = 1
-    SUBTITLE_FONT_SIZE = pptx.util.Pt(24)  # type: ignore
-
-    # FIELDS
-    TITLE = "title"
-    PART = "part"
-    CATEGORY = "category"
-    INDEX = "index"
-    DATE = "date"
-    LAYOUT_INDEX = "layout_index"
-
-    out = pptx.Presentation(INPUT_PPTX_TEMPLATE_NAME)
-
-    out.slide_width = PPTX_16_9_EMU[0]
-    out.slide_height = PPTX_16_9_EMU[1]
+    presentation = pptx.Presentation(INPUT_PPTX_TEMPLATE_NAME)
+    presentation.slide_width = PPTX_16_9_EMU[0]
+    presentation.slide_height = PPTX_16_9_EMU[1]
 
     df = pd.read_csv(INPUT_CSV_NAME)
+    df = preprocess(df=df)
 
-    df[PART] = pd.to_numeric(df[PART])
-    df[INDEX] = pd.to_numeric(df[INDEX])
-    df[DATE] = pd.to_datetime(df[DATE])
-
+    content_builder = ContentBuilder(organization_name=ORGANIZATION_NAME)
     for _, row in df.iterrows():
         layout_index = row[LAYOUT_INDEX]
-        slide_layout = out.slide_layouts[layout_index]
-        slide = out.slides.add_slide(slide_layout)
+        slide_layout = presentation.slide_layouts[layout_index]
+        slide = presentation.slides.add_slide(slide_layout)
+        content = content_builder.build_content(row=row)
+        add_content_to_slide(content=content, slide=slide)
 
-        # TITLE
-        part = row[PART]
-        if not pd.isna(part):
-            part = int(part)
-            part_content = f" (Part {part:d})"
-        else:
-            part_content = ""
-
-        title = row[TITLE]
-
-        title_content = textwrap.dedent(
-            f"""
-            {title}{part_content}
-            """
-        ).strip()
-        title_placeholder = slide.placeholders[TITLE_PLACEHOLDER_INDEX]
-        title_placeholder.text = title_content
-        for paragraph in title_placeholder.text_frame.paragraphs:
-            for run in paragraph.runs:
-                run.font.size = TITLE_FONT_SIZE
-
-        # SUBTITLE
-        category = row[CATEGORY]
-        if not pd.isna(category):
-            category_content = category
-        else:
-            category_content = ""
-
-        index = row[INDEX]
-        if not pd.isna(index):
-            index = int(index)
-            index_content = f" #{index:d}"
-        else:
-            index_content = ""
-
-        date = row[DATE].strftime(r"%Y-%m-%d")
-
-        subtitle_content = textwrap.dedent(
-            f"""
-            {category_content}{index_content}
-            {ORGANIZATION_NAME}
-            {date}
-            """
-        ).strip()
-        subtitle_placeholder = slide.placeholders[SUBTITLE_PLACEHOLDER_INDEX]
-        subtitle_placeholder.text = subtitle_content
-        for paragraph in subtitle_placeholder.text_frame.paragraphs:
-            for run in paragraph.runs:
-                run.font.size = SUBTITLE_FONT_SIZE
-
-    out.save(OUTPUT_PPTX_NAME)
+    presentation.save(OUTPUT_PPTX_NAME)
 
 
 if __name__ == "__main__":
